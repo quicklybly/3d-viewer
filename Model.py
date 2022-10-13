rotation fix

Model.py
@@ -47,24 +47,25 @@ class Model(QObject):
         self.update_model_signal()
 
     def rotate(self, axis, angle):
+        angle = np.radians(angle)
         if axis == 0:
             transition_matrix = np.array([
                 [1, 0, 0, 0],
-                [0, np.cos(angle), -1 * np.sin(angle), 0],
-                [0, np.sin(angle), np.cos(angle), 0],
+                [0, np.cos(angle), np.sin(angle), 0],
+                [0, -1 * np.sin(angle), np.cos(angle), 0],
                 [0, 0, 0, 1]
             ])
         elif axis == 1:
             transition_matrix = np.array([
-                [np.cos(angle), 0, np.sin(angle), 0],
+                [np.cos(angle), 0, -1 * np.sin(angle), 0],
                 [0, 1, 0, 0],
-                [-1 * np.sin(angle), 0, np.cos(angle), 0],
+                [np.sin(angle), 0, np.cos(angle), 0],
                 [0, 0, 0, 1]
             ])
         else:
             transition_matrix = np.array([
-                [np.cos(angle), -1 * np.sin(angle), 0, 0],
-                [np.sin(angle), np.cos(angle), 0, 0],
+                [np.cos(angle), np.sin(angle), 0, 0],
+                [-1 * np.sin(angle), np.cos(angle), 0, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, 1]
             ])
@@ -96,8 +97,10 @@ class Model(QObject):
         line_element = False
         for line in file.readlines():
             if re.fullmatch("v ([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?( )?){3}(1)?[ \n]?", line):
-                #TODO parse v 1 1 1 1
-                vertexes = np.append(vertexes, [list(map(float, line[1:].split()))], axis=0)
+                if re.fullmatch("v ([-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?( )?){3}[ \n]?", line):
+                    vertexes = np.append(vertexes, [list(map(float, line[1:].split()))], axis=0)
+                else:
+                    vertexes = np.append(vertexes, [list(map(float, line[1:].split()))[:-1]], axis=0)
             elif re.fullmatch("vt (([0-1]+([.][0-9]*)?|[.][0-9]+)( )?){3}[ \n]?", line):
                 texture = np.append(texture, [list(map(float, line[2:].split()))[:2]], axis=0)
             elif re.fullmatch("vt ([+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)( )?){3}[ \n]?", line):